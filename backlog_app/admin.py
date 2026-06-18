25d0bb468db35e2e3d927c6ba642f41a7fc85116
Ronnie95 <t_daron@yahoo.com>
2026-06-18 12:15:15 -0400

    Models registered to admin

--- backlog_app/admin.py
+++ backlog_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import projects, ticket_submissions
 
 # Register your models here.
+
+
+admin.site.register(projects),
+admin.site.register(ticket_submissions),
