703677cb93c207b578ab4d3d96fc76bc04dfd419
Ronnie95 <t_daron@yahoo.com>
2026-06-18 03:14:28 -0400

    updates to models before migration

--- backlog_app/models.py
+++ backlog_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class ticket_submissions(models.Model):
+    date = models.DateField()
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    priority_choices = (
+
+        ("LOW", "low"),
+        ("MEDIUM", "medium"),
+        ("HIGH", "high"),
+    )
+#add user related fields
+    def __str__(self):
+        return self.date
+
+
+class projects(models.Model):
+    date = models.DateField()
+    project_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    dueDate = models.DateField()
+    status_choices = (
+        ('complete', 'Complete'),
+        ('in_progress', 'In Progress'),
+        ('not_complete', 'Not Complete'),
+    )
+    status = models.CharField(max_length=20, choices=status_choices, default='not_complete')
+    #add user related field and one to many relationship
+
