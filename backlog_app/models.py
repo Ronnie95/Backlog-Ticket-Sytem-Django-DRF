c7c950e13a45bf264a6e0cb77f546c9a7ec4cbc6
Ronnie95 <t_daron@yahoo.com>
2026-06-18 23:58:29 -0400

    additions

--- backlog_app/models.py
+++ backlog_app/models.py
@@ -2,26 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-class ticket_submissions(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    date = models.DateField()
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
-    priority_choices = (
-
-        ("LOW", "low"),
-        ("MEDIUM", "medium"),
-        ("HIGH", "high"),
-    )
-    priority = models.CharField(max_length=20, choices=priority_choices, default='Low')
-
-    def __str__(self):
-        return self.date
-
-
 class projects(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     date = models.DateField()
     project_name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
@@ -32,6 +14,32 @@ class projects(models.Model):
         ('not_complete', 'Not Complete'),
     )
     status = models.CharField(max_length=20, choices=status_choices, default='not_complete')
-    tickets = models.ForeignKey(ticket_submissions, on_delete=models.CASCADE, related_name="projects") #watch for this
 
     #add the models to admin.py when complete 
+
+
+# Create your models here.
+class ticket_submissions(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    date = models.DateField()
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    priority_choices = (
+
+        ("LOW", "low"),
+        ("MEDIUM", "medium"),
+        ("HIGH", "high"),
+    )
+    priority = models.CharField(max_length=20, choices=priority_choices, default='Low')
+    project = models.ForeignKey(projects, on_delete=models.CASCADE, related_name="projects") #watch for this
+
+
+    def __str__(self):
+        return self.date
+
+
+class Comments(models.Model):
+    ticket_submission = models.ForeignKey(ticket_submissions, on_delete=models.CASCADE, related_name="ticket_submission") #watch for this
+    authors = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
