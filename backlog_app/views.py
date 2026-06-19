cf22e7bf51b9d15b9df5620316462c9876b4de2a
Ronnie95 <t_daron@yahoo.com>
2026-06-19 00:29:36 -0400

    viewsets added

--- backlog_app/views.py
+++ backlog_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Projects, Comments, Ticket_submissions
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProjectSerializer, Ticket_submissions, CommentsSerializer
+
 
 # Create your views here.
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class CommentsViewSet(ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+
+class TicketViewSet(ModelViewSet):
+    queryset = Ticket_submissions.objects.all()
+    serializer_class = Ticket_submissions
