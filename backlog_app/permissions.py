3898b08ed6de1c0c87de021803cfac18b2d8525b
Ronnie95 <t_daron@yahoo.com>
2026-06-20 21:58:03 -0400

    updates permissions

--- backlog_app/permissions.py
+++ backlog_app/permissions.py
@@ -1,2 +1,29 @@
 from rest_framework.permissions import BasePermission
 
+
+
+class IsSupervisor(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.userprofile.role == "SUPERVISOR"
+        )
+
+
+class IsSoftwareEngineer(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.userprofile.role == "Software Engineer"
+        )
+    
+
+class IsTeamMember(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.userprofile.role == "Team Member"
+        )
