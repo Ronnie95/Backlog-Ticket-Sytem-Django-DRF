e5d8d5fcbb42a44adfd18c3587a8742df74b2066
Ronnie95 <t_daron@yahoo.com>
2026-06-20 22:08:32 -0400

    updates

--- backlog_app/views.py
+++ backlog_app/views.py
@@ -3,6 +3,9 @@ from .models import Projects, Comments, Ticket_submissions, User
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ProjectSerializer, TicketSerializer, CommentsSerializer, RegisterSerializer
 from rest_framework import generics
+from .permissions import IsSoftwareEngineer, IsSupervisor, IsTeamMember
+from rest_framework.permissions import IsAuthenticated
+
 
 
 
@@ -18,13 +21,38 @@ class UserViewSet(ModelViewSet):
 class ProjectViewSet(ModelViewSet):
     queryset = Projects.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsSupervisor]
 
 
 class CommentsViewSet(ModelViewSet):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
+    permission_classes = [IsSoftwareEngineer]
+    permission_class = [IsTeamMember]
 
+
+
+# class TicketViewSet(ModelViewSet):
+#     queryset = Ticket_submissions.objects.all()
+#     serializer_class = TicketSerializer
+#     permission_classes = [IsTeamMember]
 
 class TicketViewSet(ModelViewSet):
-    queryset = Ticket_submissions.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        role = user.userprofile.role
+
+        if role == "SUPERVISOR":
+            return Ticket_submissions.objects.all()
+
+        if role == "SWE":
+            return Ticket_submissions.objects.filter(
+                assigned_to=user
+            )
+
+        return Ticket_submissions.objects.filter(
+            created_by=user
+        )
