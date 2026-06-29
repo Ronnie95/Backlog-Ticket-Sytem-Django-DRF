3c46d1e001b5a22350cd7cf58b507ccf55107072
Ronnie95 <t_daron@yahoo.com>
2026-06-28 20:19:17 -0400

    updates

--- backlog_app/serializers.py
+++ backlog_app/serializers.py
@@ -53,6 +53,37 @@ class RegisterSerializer(ModelSerializer):
         )
 
         return user
+    
+
+    class LoginSerializer(ModelSerializer):
+
+    role = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "password",
+            
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+
+        role = validated_data.pop("role")
+
+        user = User.objects.create_user(
+            **validated_data
+        )
+
+        UserProfile.objects.create(
+            user=user,
+            role=role
+        )
+
+        return user
 
 class TicketSerializer(ModelSerializer):
     class Meta:
